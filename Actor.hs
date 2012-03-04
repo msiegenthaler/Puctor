c66df7ee6e8d66572c5acd7e86188ebbcedcf978
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-03-04 14:01:51 +0100

    refactored Actor

--- Actor.hs
+++ Actor.hs
@@ -1,82 +1,93 @@
-{-# LANGUAGE ExistentialQuantification, FlexibleInstances #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 module Control.Concurrent.Puctor.Actor (
-  Actor,
-  ActorId,
-  ActorFactory,
-  ActorCreator,
-  actorId,
-  (!),
-  sendMsg,
-  newActorId,
-  splitActorFactory,
-  Message(..),
-  ActorSpawn(..),
-  Effect(..),
-  Effects,
-  initialActor,
-  boot
+    ActorEnv,
+    newEnv,
+    splitEnv,
+    newActor,
+    ActorCreate,
+    send,
+    Effect    
 ) where
 
+import Data.Tuple
+import Data.Maybe
 import Data.Unique.Id
 import Control.Monad.State
+import Control.Applicative
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Concurrent.STM
+import Unsafe.Coerce
 
-
-newtype ActorId = ActorId Id deriving (Eq, Ord)
-instance Show ActorId where
-  show (ActorId id) = "<" ++ show id ++ ">"
-    where num = tail $ show id
 
 class Actor actor where
-    actorId :: actor msg -> ActorId
-    (!) :: actor msg -> msg -> IO ()
-
-{- TODO
-instance (Actor a) => Show (a b) where
-    show = ("Actor "++) . show . actorId
-instance (Actor a) => Eq (a b) where
-    a == b = (actorId a) == (actorId b)
--}
-
-newtype ActorFactory = ActorFactory IdSupply
-
-data Message actor msg = Message  { to :: actor msg
-                                  , msg :: msg }
-
-data ActorSpawn a = ActorSpawn { actor       :: a
-                               , spawnAction :: IO ()}                            
+    sendMsg :: msg -> actor msg -> IO ()
 
 
-data Effect = forall actor msg. Actor actor => Send (Message actor msg)
-            | forall actor msg. Actor actor => Spawn (ActorSpawn (actor msg))
-type Effects = [Effect]
+type ActorCreate actor msg = ActorRef msg -> IO (actor msg)
+type Effect = ActorEnv -> IO ()
 
 
-type ActorCreator a = ActorFactory -> (ActorFactory, ActorSpawn a)
+data ActorRef msg = ActorRef Id deriving Eq
 
-newActorId :: ActorFactory -> (ActorId, ActorFactory)
-newActorId af = idFromActorFactory `mapfst` (splitActorFactory af)
-
-mapfst f (a,b) = (f a, b)
-
-idFromActorFactory :: ActorFactory -> ActorId
-idFromActorFactory (ActorFactory ids) = ActorId $ idFromSupply ids
-
-splitActorFactory :: ActorFactory -> (ActorFactory, ActorFactory)
-splitActorFactory (ActorFactory ids) = (ActorFactory idsa, ActorFactory idsb)
-    where (idsa, idsb) = splitIdSupply ids
-  
-actorFactory :: IO ActorFactory
-actorFactory = ActorFactory `liftM` initIdSupply 'A'
+instance Show (ActorRef msg) where
+    show (ActorRef id) = "<" ++ (tail $ show id) ++ ">"
 
 
-sendMsg to msg = Send $ Message to msg
 
-initialActor :: Actor a => ActorCreator (a msg) -> IO (a msg)
-initialActor c = do
-  s <- (snd . c) `liftM` actorFactory
-  spawnAction s
-  return (actor s)
+data ActorBox msg = forall a. Actor a => ActorBox (a msg)
+instance Actor ActorBox where
+    sendMsg msg (ActorBox a) = sendMsg msg a
 
-boot :: Actor a => ActorCreator (a ()) -> IO ()
-boot c = initialActor c >>= (flip (!)) () >> return ()
+data NoopActor msg = NoopActor
+instance Actor NoopActor where
+    sendMsg _ _ = return ()
+
+data ActorBoxU = forall msg. ActorBoxU msg
+type ActorMap = Map Id ActorBoxU
+
+data ActorEnv = ActorEnv { actorMap  :: TVar ActorMap
+                         , idSupply  :: IdSupply
+                         , deferred  :: [ActorEnv -> IO ActorEnv]}
+
+
+
+newEnv :: IO ActorEnv
+newEnv = ActorEnv <$> newTVarIO Map.empty <*> initIdSupply 'A' <*> return []
+
+splitEnv :: ActorEnv -> (ActorEnv, ActorEnv)
+splitEnv env = ((mapBoth $ updateIdSupply env) . splitIdSupply . idSupply) env
+mapBoth f (a, b) = (f a, f b)
+updateIdSupply env s = env { idSupply = s }
+
+
+newActor :: Actor actor => ActorCreate actor msg -> ActorEnv -> (ActorRef msg, ActorEnv)
+newActor f e = (ref, addTo e')
+    where (ref, e') = newActorRef e
+          addTo = addDeferred $ createActor f ref
+addDeferred f env = env { deferred = deferred env ++ [f] }
+
+createActor :: Actor actor => ActorCreate actor msg -> ActorRef msg -> ActorEnv -> IO ActorEnv
+createActor f ref env = f ref >>= registerActor env ref >> return env
+registerActor env (ActorRef aid) a = atomically $ do
+        m <- readTVar v
+        let m' = Map.insert aid (box a) m
+        writeTVar v m'
+    where v = actorMap env
+
+newActorRef :: ActorEnv -> (ActorRef msg, ActorEnv)
+newActorRef = swap . fmap create . splitEnv
+    where create = ActorRef . idFromSupply . idSupply
+
+
+
+
+send :: ActorRef msg -> msg -> Effect
+send (ActorRef aid) msg env = lookupActor aid <$> readTVarIO (actorMap env) >>= sendMsg msg
+lookupActor aid = fromMaybe NoopActor . fmap unbox . Map.lookup aid
+
+box :: Actor a => a msg -> ActorBoxU
+box = ActorBoxU . ActorBox
+unbox :: Actor a => ActorBoxU -> a msg
+unbox (ActorBoxU box) = unsafeCoerce box
